0593aa1729d22197e6e41606713541e95701ca60
james-coulter <95coulter@gmail.com>
2020-07-22 12:11:46 -0600

    Finished first three challenges. Not the most optimized

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,22 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+    nonZero = 0
+    l = len(arr)
+
+    for i in range(l):
+        if arr[i] != 0:
+            arr[nonZero] = arr[i]
+            nonZero += 1
+
+    while nonZero < l:
+        arr[nonZero] = 0
+        nonZero += 1
+
+    return arr
 
 
 if __name__ == '__main__':
